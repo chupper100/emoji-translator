0c4e71c3161226eb2ac4e31edfbc8ac3179a8ce6
chupper100 <phamminhdat223@gmail.com>
2021-08-06 12:39:16 +0700

    function text_emoji() worked

--- main.py
+++ main.py
@@ -31,17 +31,15 @@ def choose_mode():
     user_choice=input('ETT: Emoji To Text, TTE: Text To Emoji ').lower()
     return user_choice
 
-output_text=''
 def text_emoji(): #Text To Emoji
     output_text=''
     input_text=str(input('Input:')).lower()
     for letter in input_text:
         if letter in emoji:
             output_text=output_text+emoji[letter]
-            print(i, output_text)
-            i=i+1
         else:
             output_text=output_text+letter
+    return output_text
 
 def emoji_text(): #Emoji To Text
     input_text=str(input('Input:')).lower()
@@ -50,12 +48,14 @@ def emoji_text(): #Emoji To Text
 def main():
     choice=choose_mode()
     if choice=='ett':
-        print('You choosed to translate emoji to text.')
-        emoji_text()
+        print('You have choosed to translate emoji to text.')
+        print(emoji_text())
 
     elif choice=='tte':
-        print('You choosed to translate text to emoji.')
-        text_emoji()
+        print('You have choosed to translate text to emoji.')
+        print(text_emoji())
+    else:
+        print('Please choose a valid option!')
+        main()
 
 main()
-print(output_text)
