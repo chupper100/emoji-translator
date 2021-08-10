830893c10fc604ff138e7bc2a9d595d98d1f2bc5
chupper100 <phamminhdat223@gmail.com>
2021-08-10 16:03:18 +0700

    formated function text_emoji()

--- main.py
+++ main.py
@@ -27,35 +27,31 @@ emoji={
 'z':'😏️',
 }
 
-def text_emoji(): #Text To Emoji
-    output_text=''
+def text_emoji(emoji): #Text To Emoji
     input_text=str(input('Input:')).lower()
-    for letter in input_text:
-        if letter in emoji:
-            output_text=output_text+emoji[letter]
-        else:
-            output_text=output_text+letter
+    table = str.maketrans(emoji)
+
+    output_text = input_text.translate(table)
     return output_text
 
 def emoji_text(emoji): #Emoji To Text
-    output_text=''
-    emoji_to_letter = {v.strip('\ufe0f'): k for k, v in emoji.items()}
+    input_text = input()
+    emoji_to_letter = {v.strip('\ufe0f'): k for k, v in emoji.items()} #reserve
     table = str.maketrans(emoji_to_letter)
 
-    input_text = input()
     output_text = input_text.translate(table)
     return output_text
 
 def main(emoji):
-    choice=input('ETT: Emoji To Text, TTE: Text To Emoji ').lower()
-    if choice=='ett' or choice=='emoji to text':
+    choice=input('What do you want to translate to? ').lower()
+    if choice=='text' or choice=='t':
         print('You have choosed to translate emoji to text.')
         print(emoji_text(emoji))
-    elif choice=='tte' or choice=='text to emoji':
+    elif choice=='emoji' or choice=='e':
         print('You have choosed to translate text to emoji.')
-        print(text_emoji())
+        print(text_emoji(emoji))
     else:
         print('Please choose a valid option!')
-        main()
+        main(emoji)
 
 main(emoji)
